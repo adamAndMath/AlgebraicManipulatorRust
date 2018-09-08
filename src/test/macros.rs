040d8955efccfb1a8ae307469a22d6e851c36942
Adam <adam.and.math@gmail.com>
2018-09-08 18:35:13 +0200

    Removed unnecessary macro cases

--- src/test/macros.rs
+++ src/test/macros.rs
@@ -5,13 +5,10 @@ macro_rules! ttype {
 }
 
 macro_rules! ttype_vec {
-    (($($v:tt)*), ) => (vec!($($v)*));
-    (($($v:tt)*), $t:ident) => (ttype_vec!(($($v)* ttype!($t)), ));
-    (($($v:tt)*), $t:ident[$($g:tt)*]) => (ttype_vec!(($($v)* ttype!($t[$($g)*])), ));
-    (($($v:tt)*), ($($t:tt)*)) => (ttype_vec!(($($v)* ttype!(($($t),*))), ));
-    (($($v:tt)*), $t:ident, $($rest:tt)*) => (ttype_vec!(($($v)* ttype!($t), ), $($rest)*));
-    (($($v:tt)*), $t:ident[$($g:tt)*], $($rest:tt)*) => (ttype_vec!(($($v)* ttype!($t[$($g)*])), $($rest)*));
-    (($($v:tt)*), ($($p:tt)*), $($rest:tt)*) => (ttype_vec!(($($v)* ttype!(($($p),*))), $($rest)*));
+    (($($v:tt)*)$(,)* ) => (vec!($($v)*));
+    (($($v:tt)*), $t:ident $($rest:tt)*) => (ttype_vec!(($($v)* ttype!($t), ) $($rest)*));
+    (($($v:tt)*), $t:ident[$($g:tt)*] $($rest:tt)*) => (ttype_vec!(($($v)* ttype!($t[$($g)*])) $($rest)*));
+    (($($v:tt)*), ($($p:tt)*) $($rest:tt)*) => (ttype_vec!(($($v)* ttype!(($($p),*))) $($rest)*));
 }
 
 macro_rules! type_id {
@@ -21,36 +18,24 @@ macro_rules! type_id {
 }
 
 macro_rules! type_id_gen {
-    (($($v:tt)*), ) => (vec!($($v)*));
-    (($($v:tt)*), $t:ident) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!($t))), ));
-    (($($v:tt)*), +$t:ident) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!($t))), ));
-    (($($v:tt)*), -$t:ident) => (type_id_vec!(($($v)* (Variance::Contravariant, type_id!($t))), ));
-    (($($v:tt)*), $t:ident[$($g:tt)*]) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!($t[$($g)*]))), ));
-    (($($v:tt)*), +$t:ident[$($g:tt)*]) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!($t[$($g)*]))), ));
-    (($($v:tt)*), -$t:ident[$($g:tt)*]) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!($t[$($g)*]))), ));
-    (($($v:tt)*), ($($t:tt)*)) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!(($($t),*)))), ));
-    (($($v:tt)*), +($($t:tt)*)) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!(($($t),*)))), ));
-    (($($v:tt)*), -($($t:tt)*)) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!(($($t),*)))), ));
-    (($($v:tt)*), $t:ident, $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!($t)), ), $($rest)*));
-    (($($v:tt)*), +$t:ident, $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!($t)), ), $($rest)*));
-    (($($v:tt)*), -$t:ident, $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!($t)), ), $($rest)*));
-    (($($v:tt)*), $t:ident[$($g:tt)*], $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!($t[$($g)*]))), $($rest)*));
-    (($($v:tt)*), +$t:ident[$($g:tt)*], $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!($t[$($g)*]))), $($rest)*));
-    (($($v:tt)*), -$t:ident[$($g:tt)*], $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!($t[$($g)*]))), $($rest)*));
-    (($($v:tt)*), ($($p:tt)*), $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!(($($p),*)))), $($rest)*));
-    (($($v:tt)*), +($($p:tt)*), $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!(($($p),*)))), $($rest)*));
-    (($($v:tt)*), -($($p:tt)*), $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!(($($p),*)))), $($rest)*));
+    (($($v:tt)*)$(,)* ) => (vec!($($v)*));
+    (($($v:tt)*), $t:ident $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!($t)), ) $($rest)*));
+    (($($v:tt)*), +$t:ident $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!($t)), ) $($rest)*));
+    (($($v:tt)*), -$t:ident $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!($t)), ) $($rest)*));
+    (($($v:tt)*), $t:ident[$($g:tt)*] $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!($t[$($g)*]))) $($rest)*));
+    (($($v:tt)*), +$t:ident[$($g:tt)*] $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!($t[$($g)*]))) $($rest)*));
+    (($($v:tt)*), -$t:ident[$($g:tt)*] $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!($t[$($g)*]))) $($rest)*));
+    (($($v:tt)*), ($($p:tt)*) $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Invariant, type_id!(($($p),*)))) $($rest)*));
+    (($($v:tt)*), +($($p:tt)*) $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Covariant, type_id!(($($p),*)))) $($rest)*));
+    (($($v:tt)*), -($($p:tt)*) $($rest:tt)*) => (type_id_gen!(($($v)* (Variance::Contravariant, type_id!(($($p),*)))) $($rest)*));
 }
 
 macro_rules! type_id_tuple {
-    ((type_id!($($e:tt)*)), ) => (type_id!($($e)*));
-    (($($v:tt)*), ) => (TypeID::Tuple(vec!($($v)*)));
-    (($($v:tt)*), $t:ident) => (type_id_tuple!(($($v)* type_id!($t)), ));
-    (($($v:tt)*), $t:ident[$($g:tt)*]) => (type_id_tuple!(($($v)* type_id!($t[$($g)*])), ));
-    (($($v:tt)*), ($($t:tt)*)) => (type_id_tuple!(($($v)* type_id!(($($t),*))), ));
-    (($($v:tt)*), $t:ident, $($rest:tt)*) => (type_id_tuple!(($($v)* type_id!($t), ), $($rest)*));
-    (($($v:tt)*), $t:ident[$($g:tt)*], $($rest:tt)*) => (type_id_tuple!(($($v)* type_id!($t[$($g)*])), $($rest)*));
-    (($($v:tt)*), ($($p:tt)*), $($rest:tt)*) => (type_id_tuple!(($($v)* type_id!(($($p),*))), $($rest)*));
+    ((type_id!($($e:tt)*))$(,)* ) => (type_id!($($e)*));
+    (($($v:tt)*)$(,)* ) => (TypeID::Tuple(vec!($($v)*)));
+    (($($v:tt)*), $t:ident $($rest:tt)*) => (type_id_tuple!(($($v)* type_id!($t), ) $($rest)*));
+    (($($v:tt)*), $t:ident[$($g:tt)*] $($rest:tt)*) => (type_id_tuple!(($($v)* type_id!($t[$($g)*])) $($rest)*));
+    (($($v:tt)*), ($($p:tt)*) $($rest:tt)*) => (type_id_tuple!(($($v)* type_id!(($($p),*))) $($rest)*));
 }
 
 macro_rules! exp {
@@ -60,12 +45,10 @@ macro_rules! exp {
 }
 
 macro_rules! exp_tuple {
-    ((exp!($($e:tt)*)), ) => (exp!($($e)*));
-    (($($v:tt)*), ) => (Exp::Tuple(vec!($($v)*)));
-    (($($v:tt)*), $x:ident$(($($p:tt)*))*) => (exp_tuple!(($($v)* exp!($x$(($($p)*))*)), ));
-    (($($v:tt)*), $(($($p:tt)*))+) => (exp_tuple!(($($v)* exp!($(($($p)*))*)), ));
-    (($($v:tt)*), $x:ident$(($($p:tt)*))*, $($rest:tt)*) => (exp_tuple!(($($v)* exp!($x$(($($p)*))*)), $($rest)*));
-    (($($v:tt)*), $(($($p:tt)*))+, $($rest:tt)*) => (exp_tuple!(($($v)* exp!($(($($p)*))*)), $($rest)*));
+    ((exp!($($e:tt)*))$(,)* ) => (exp!($($e)*));
+    (($($v:tt)*)$(,)* ) => (Exp::Tuple(vec!($($v)*)));
+    (($($v:tt)*), $x:ident$(($($p:tt)*))* $($rest:tt)*) => (exp_tuple!(($($v)* exp!($x$(($($p)*))*)) $($rest)*));
+    (($($v:tt)*), $(($($p:tt)*))+ $($rest:tt)*) => (exp_tuple!(($($v)* exp!($(($($p)*))*)) $($rest)*));
 }
 
 macro_rules! exp_id {
@@ -75,10 +58,8 @@ macro_rules! exp_id {
 }
 
 macro_rules! exp_id_tuple {
-    ((exp_id!($($e:tt)*)), ) => (exp_id!($($e)*));
+    ((exp_id!($($e:tt)*))$(,)* ) => (exp_id!($($e)*));
     (($($v:tt)*), ) => (ExpID::Tuple(vec!($($v)*)));
-    (($($v:tt)*), $x:ident$(($($p:tt)*))*) => (exp_id_tuple!(($($v)* exp_id!($x$(($($p)*))*)), ));
-    (($($v:tt)*), $(($($p:tt)*))+) => (exp_id_tuple!(($($v)* exp_id!($(($($p)*))*)), ));
-    (($($v:tt)*), $x:ident$(($($p:tt)*))*, $($rest:tt)*) => (exp_id_tuple!(($($v)* exp_id!($x$(($($p)*))*)), $($rest)*));
-    (($($v:tt)*), $(($($p:tt)*))+, $($rest:tt)*) => (exp_id_tuple!(($($v)* exp_id!($(($($p)*))*)), $($rest)*));
+    (($($v:tt)*), $x:ident$(($($p:tt)*))* $($rest:tt)*) => (exp_id_tuple!(($($v)* exp_id!($x$(($($p)*))*)) $($rest)*));
+    (($($v:tt)*), $(($($p:tt)*))+ $($rest:tt)*) => (exp_id_tuple!(($($v)* exp_id!($(($($p)*))*)) $($rest)*));
 }
