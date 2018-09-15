20235b4bae32d96cc2ec40c9519c12d9c0adafd2
Adam <adam.and.math@gmail.com>
2018-09-15 18:45:01 +0200

    Adding type scoping methods

--- src/envs.rs
+++ src/envs.rs
@@ -91,14 +91,28 @@ impl<'a> LocalEnvs<'a> {
     pub fn scope<'b>(&'b self, v: Vec<(String, ExpVal)>) -> LocalEnvs<'b> where 'a: 'b {
         LocalEnvs {
             exp: self.exp.scope(v),
-            ty: self.ty.scope(vec!()),
+            ty: self.ty.scope(vec![]),
+        }
+    }
+
+    pub fn scope_ty<'b>(&'b self, v: Vec<(String, TypeVal)>) -> LocalEnvs<'b> where 'a: 'b {
+        LocalEnvs {
+            exp: self.exp.scope(vec![]),
+            ty: self.ty.scope(v),
         }
     }
     
     pub fn scope_anon<'b>(&'b self, v: Vec<ExpVal>) -> LocalEnvs<'b> where 'a: 'b {
         LocalEnvs {
             exp: self.exp.scope_anon(v),
-            ty: self.ty.scope_anon(vec!()),
+            ty: self.ty.scope_anon(vec![]),
+        }
+    }
+    
+    pub fn scope_ty_anon<'b>(&'b self, v: Vec<TypeVal>) -> LocalEnvs<'b> where 'a: 'b {
+        LocalEnvs {
+            exp: self.exp.scope_anon(vec![]),
+            ty: self.ty.scope_anon(v),
         }
     }
 }
